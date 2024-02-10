fdb625e71214060cb1ba0edb9cf0bd20a4b3f6e4
SzanieckiFranciszek <franekszaniecki@gmail.com>
2024-02-10 23:49:41 +0100

    Initial commit

---
+++ src/main/java/com/franciszekszaniecki/kafkaspringbootlearnproject/KafkaSpringbootLearnProjectApplication.java
@@ -0,0 +1,13 @@
+package com.franciszekszaniecki.kafkaspringbootlearnproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringbootLearnProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringbootLearnProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/franciszekszaniecki/kafkaspringbootlearnproject/KafkaSpringbootLearnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciszekszaniecki.kafkaspringbootlearnproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringbootLearnProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
